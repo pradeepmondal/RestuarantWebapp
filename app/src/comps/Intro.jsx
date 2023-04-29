ecd95f3cb249624395abe41fa25788bde9bb79dd
pradeepmondal <mail.pradeepmondal@gmail.com>
2023-04-29 20:16:10 +0530

    added gradient

--- app/src/comps/Intro.jsx
+++ app/src/comps/Intro.jsx
@@ -8,22 +8,23 @@ import {BsBalloonHeartFill} from 'react-icons/bs'
 
 export default function Intro(){
 
-
-   
-
     const slides = [
         {url: 'https://images.pexels.com/photos/406152/pexels-photo-406152.jpeg'},
         {url: 'https://images.pexels.com/photos/4078054/pexels-photo-4078054.jpeg'},
         {url: 'https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg'}
     ]
 
-    const [slideIndex, setSlideIndex] = useState(0)
+
+    const [slideIndex, setSlideIndex] = useState(0);
+    
 
     async function slideChange(){
         
          await new Promise( (resolve) => {setTimeout(() => {
             if(slideIndex !== (slides.length - 1)){
+                
                 setSlideIndex(slideIndex + 1);
+                
                 
                  
             }
@@ -63,10 +64,10 @@ export default function Intro(){
     
 
     return (
-        <div className='w-[100%] h-[100vh] sm:h-[100vh] bg-gray-900'>
+        <div className='w-[100%] h-[100vh] sm:h-[100vh] bg-gradient-to-t from-violet-900 from-0% via-slate-950 via-15% to-yellow-700 to-[92%]'>
             
             <div className='max-w-[90%] sm:max-w-[90%] h-[75vh] sm:h-[75vh] w-full mx-auto pt-[14vh]  z-0 '>
-                <div style={{backgroundImage: `url(${slides[slideIndex].url})`}} className='w-full h-full bg-center bg-cover rounded-3xl duration-500 border-4 border-yellow-700/90 shadow-md shadow-yellow-700'>
+                <div style={{backgroundImage: `url(${slides[slideIndex].url})`}} className='w-full h-full bg-center bg-cover rounded-3xl duration-500 border-4 border-yellow-700/90 shadow-md shadow-yellow-700 origin-left'>
                 </div>
                 <h1 className='flex flex-row text-2xl sm:text-6xl text-slate-300 pt-[8vh] max-w-fit mx-auto '> We <BsBalloonHeartFill className='text-4xl sm:text-[5rem] text-red-500 mx-2 sm:mx-4  animate-pulse '/> Food !</h1>
 
