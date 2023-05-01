27bb8f5f32a6177817763c630ed1821a418c410b
pradeepmondal <mail.pradeepmondal@gmail.com>
2023-05-01 20:22:57 +0530

    added basic starting animation

--- app/src/comps/StartAnimation.jsx
+++ app/src/comps/StartAnimation.jsx
@@ -1,12 +1,79 @@
 import React from 'react';
+import { useRef, useEffect } from 'react';
+import Typewriter from 'typewriter-effect';
 
 export default function StartAnimation() {
-    return (
-        <div className='fixed w-0 h-[100vh] bg-yellow-700/95 flex-col place-items-center justify-center tracking-[1px] py-40  text-2xl origin-left duration-500 '>
+
+    const animCover = useRef('');
+    const typeDiv = useRef('');
+    
+
+    console.log(animCover.current);
+
+    function disAnim() {
+        
+        setTimeout(() => {
+
+        animCover.current.classList.remove('appearH');
+        typeDiv.current.classList.add('disappear');
+        
+
+        
+
+
+
+    }, 11000);
+
+    }
+
+    useEffect(() => {
+        disAnim();
+      }, []);
+      
+
+
+
+     return (
+
+        <div ref = {animCover} className='appearH  w -full h-[0px] text-[3rem] sm:text-[5rem] bg-gray-950 flex flex-row text-slate-200 place-items-center justify-center tracking-[1px]  origin-top duration-1000 z-30'> 
+
+
+        <div ref = {typeDiv} className=''>
+
+        <Typewriter className=' mx-auto'
+                onInit={(typewriter) => {
+                typewriter.typeString('Welcome to Foody')
+                .callFunction(() => {
+                    console.log('String typed out!');
+                })
+                .pauseFor(1000)
+                .deleteAll()
+                typewriter.typeString("Let's dive in ")
+                .pauseFor(400)
+                typewriter.typeString(". ")
+                .pauseFor(400)
+                typewriter.typeString(". ")
+                .pauseFor(400)
+                typewriter.typeString(".")
+                
+                .callFunction(() => {
+                    console.log('All strings were deleted');
+                })
+                .start();
+                }}
+            />
+
+       
+
+
+
+        </div>
+            
+
             
 
             
         </div>
-    );
-};
+    )
+}
 
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Nav from '@/comps/Nav'
 import Intro from '@/comps/Intro'
 import Story from '@/comps/Story'
+import StartAnimation from '@/comps/StartAnimation'
 
 
 
@@ -11,6 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
+      <StartAnimation />
       <Nav />
       <Intro />
       <Story />
