38f274b4aa75850fddeb668591c4561793f4e5b4
pradeepmondal <mail.pradeepmondal@gmail.com>
2023-07-26 23:32:41 +0530

    minor respo adjustments

--- app/src/admComps/AdmLogin.jsx
+++ app/src/admComps/AdmLogin.jsx
@@ -16,15 +16,15 @@ export default function AdmLogin({fetchLog}) {
   
     return  (
     
-    <div className='fixed bg-pink-200 w-screen h-[100vh]'>
+    <div className='fixed bg-green-100 w-screen h-[100vh]'>
   
-      <div className='flex flex-col m-[12rem] sm:m-[12rem] mx-[1rem] sm:mx-[20rem] p-[1rem] sm:p-[4rem] border-4 border-pink-400 bg-pink-300 rounded-[1rem]'>
+      <div className='flex flex-col m-[12rem] sm:m-[5rem] md:m-[5rem] lg:m-[7rem] mx-[1rem] sm:px-[5rem] md:px-[15rem] p-[1rem] sm:p-[4rem] border-4 border-green-300 bg-green-300 rounded-[1rem]'>
   
-      <h1 className='flex flex-row justify-center   text-[2rem] text-pink-700'>Hello admin, login here</h1><br />
+      <h1 className='flex flex-row justify-center text-[2rem] text-green-700'>Hello admin, login here</h1><br />
   
   <div className='flex flex-row space-x-[0.1rem] sm:space-x-4'>
   
-      <div className='basis-1/2 text-[1.5rem] text-pink-600 space-y-4'>
+      <div className='basis-1/2 text-[1.5rem] text-green-600 space-y-4'>
           <label>
           Username
           </label> <br />
@@ -34,10 +34,10 @@ export default function AdmLogin({fetchLog}) {
           </label>
       </div>
   
-      <div className='basis-1/2 text-[1.5rem] text-pink-500 space-y-2'>
-          <input onChange = {(e) => {setUsername(e.target.value)}} placeholder='username' className='bg-pink-100 placeholder-pink-300 px-5' /> <br />
+      <div className='basis-1/2 text-[1.5rem] text-ggreen-500 space-y-2'>
+          <input onChange = {(e) => {setUsername(e.target.value)}} placeholder='username' className='bg-green-100 placeholder-green-300 px-5' /> <br />
   
-          <input onChange = {(e) => {setPassword(e.target.value)}} type='password' placeholder='password' className='bg-pink-100 placeholder-pink-300 px-5'>
+          <input onChange = {(e) => {setPassword(e.target.value)}} type='password' placeholder='password' className='bg-green-100 placeholder-green-300 px-5'>
           </input>
       </div>
   
@@ -46,7 +46,7 @@ export default function AdmLogin({fetchLog}) {
   
   
   <button 
-  className='mx-auto mt-[3rem] bg-pink-500 max-w-fit p-2 rounded-[0.5rem]'
+  className='mx-auto mt-[3rem] bg-green-500 max-w-fit p-2 rounded-[0.5rem]'
   onClick={async(e) => {
     console.log('button clicked');
     try{
